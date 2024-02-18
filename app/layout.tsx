e0d276d3bf25351370da6966695bb27606964b57
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-02-18 19:48:09 +0600

    feat(ui): create a basic footer
    This is a basic footer. It includes the copyright symbol, current year, and developer team name - Team Trinity.
    
    Closes #14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/shared/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen">
+          {children}
+        </div>
+        <Footer />
+      </body>
     </html>
   );
 }
