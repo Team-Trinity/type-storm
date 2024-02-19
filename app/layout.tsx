b1a24fc7dd979efa20e89845adb00012dca5b4ee
Nasif Ahmed <nasif2ahmed@gamil.com>
2024-02-19 12:20:51 +0600

    set default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" class="dark">
             <body
                 className={cn(
                     "min-h-screen bg-background font-sans antialiased",
