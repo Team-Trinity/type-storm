d5fcd1c8b9b280ed1a84443f39dd1e2578729b57
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-03-07 13:50:09 +0600

    create hooks according to new backend rewrite

--- hooks/useAxios.tsx
+++ hooks/useAxios.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 import axios from "axios";
 
 export const useAxios = () => {
     const instance = axios.create({
         // baseURL: "http://localhost:5000",
-        baseURL: "https://type-storm-server-one.vercel.app/api/v1/users",
+        baseURL: "https://type-storm-server-one.vercel.app/api/v1",
         timeout: 1000
     });
     return instance;
