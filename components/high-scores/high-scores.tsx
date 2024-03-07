42d399eee3945449d76fe33d20de1b4613de6f61
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-03-07 14:31:36 +0600

    fix stat display when new user

--- components/high-scores/high-scores.tsx
+++ components/high-scores/high-scores.tsx
@@ -1,13 +1,14 @@
-
-  // @ts-ignore
-  export const HighScores= ({ highScore, index }) => {
-    {console.log(highScore)}
+// @ts-ignore
+export const HighScores = ({ highScore, index }) => {
+    {
+        console.log(highScore);
+    }
     return (
         <tr className="">
-        <td className="py-4 px-6 border-b">{highScore.name}</td>
-        <td className="py-4 px-6 border-b">{highScore.topWpm}</td>
-        <td className="py-4 px-6 border-b">{highScore.totalCpm}</td>
-        <td className="py-4 px-6 border-b text-end">{index+1}</td>
-    </tr>
+            <td className="border-b px-6 py-4">{highScore.name}</td>
+            <td className="border-b px-6 py-4">{Math.max(highScore.topWpm)}</td>
+            <td className="border-b px-6 py-4">{highScore.totalCpm}</td>
+            <td className="border-b px-6 py-4 text-end">{index + 1}</td>
+        </tr>
     );
-  };
+};
